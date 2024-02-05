86bfb8616f7f3033d31ebc5f83e8a8736c8b850a
IkJuLim <limik1004@gmail.com>
2024-02-05 14:14:34 +0900

    first commit

---
+++ src/test/java/click/limikju/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package click.limikju.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
